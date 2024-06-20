05a9e771d6f9a027253158b4f667e548ef2b62ee
victorcreativee <tuyizerevictor@gmail.com>
2024-06-20 11:47:50 -0700

    betty test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Function to print hello world with puts function
+ * Return: value equal to 0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
