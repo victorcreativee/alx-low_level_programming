02f5076b8da9dd1cc9b92b13c831ce852b64cde3
victorcreativee <tuyizerevictor@gmail.com>
2024-06-21 11:33:36 -0700

    if condition

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A C program that prints a line to the standard error
  * Return: 1 (indicating success)
  */
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+  	  stderr);
     return (1);
 }
