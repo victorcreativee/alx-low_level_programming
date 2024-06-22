4e2baea50bd19ae81f500244c2c85047dc0d96fb
victorcreativee <tuyizerevictor@gmail.com>
2024-06-21 17:13:59 -0700

    last

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,20 @@ int main(void)
 {
 	int n = 0;
 
-	while (n <= 9)
+	while (n < 10)
 	{
 		putchar(n + '0');
 
 		if (n < 9)
 		{
-			putchar(", $"[n == 8]);
+			putchar(',');
+			putchar(' ');
 		}
+		else
+		{
+			putchar('$');
+		}
+
 		n++;
 	}
 
