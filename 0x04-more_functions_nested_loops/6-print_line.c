66c8b6819ef87d7444555659a362b5251bb5e760
victorcreativee <tuyizerevictor@gmail.com>
2024-06-27 15:14:17 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,7 +7,7 @@
 
 void print_line(int n)
 {
-	if (n > 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -15,7 +15,7 @@ void print_line(int n)
 	{
 		int i;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
